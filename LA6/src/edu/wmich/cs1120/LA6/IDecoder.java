5995cbdc8854a6ee3dc0b5a408f57bc2a6009568
Admin <Admin@DESKTOP-TC0LONV>
2018-11-21 19:39:26 -0500

    Fixed Main Class

--- LA6/src/edu/wmich/cs1120/LA6/IDecoder.java
+++ LA6/src/edu/wmich/cs1120/LA6/IDecoder.java
@@ -1,5 +1,11 @@
 package edu.wmich.cs1120.LA6;
 
 public interface IDecoder {
+	
+	// Given the file path of the binary file, the decoder will read the file, decode the message and 
+	// print it to the console.
+	// Include code to handle the IOException.
 	public void decode(String filePath);
+
+	
 }
