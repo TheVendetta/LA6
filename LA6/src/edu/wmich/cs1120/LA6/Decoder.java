ce4717bd82bac0b71f9cc2eb8fbb70369eff8d09
lucas <lucas@192.168.1.181>
2018-11-21 20:20:02 -0500

    Lucas: wrote the decoder class

--- LA6/src/edu/wmich/cs1120/LA6/Decoder.java
+++ LA6/src/edu/wmich/cs1120/LA6/Decoder.java
@@ -1,10 +1,35 @@
 package edu.wmich.cs1120.LA6;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+import java.io.*;
+
 public class Decoder implements IDecoder{
 
 	@Override
-	public void decode(String filePath) {
-		// TODO Auto-generated method stub
+	public void decode(String filePath) throws IOException {
+		String message = "";
+		int counter = 0;
+		int pos;
+		char letter;
+		try {
+			RandomAccessFile inFile = new RandomAccessFile(filePath,"r");
+			while(counter != -1) {
+				letter = inFile.readChar();
+				message = message + letter;
+				counter = inFile.readInt();
+				pos = (int) inFile.getFilePointer();
+				inFile.seek(counter + pos);
+			}
+			System.out.println(message);
+			
+		}
+		catch(FileNotFoundException E){
+			System.out.println("File not found");
+		}
+		
+		
 		
 	}
 
--- LA6/src/edu/wmich/cs1120/LA6/IDecoder.java
+++ LA6/src/edu/wmich/cs1120/LA6/IDecoder.java
@@ -1,11 +1,5 @@
 package edu.wmich.cs1120.LA6;
-
+import java.io.*;
 public interface IDecoder {
-	
-	// Given the file path of the binary file, the decoder will read the file, decode the message and 
-	// print it to the console.
-	// Include code to handle the IOException.
-	public void decode(String filePath);
-
-	
+	public void decode(String filePath) throws IOException;
 }
