55c403337d4e28c16438cba63d5911d5a2f400ad
lucas <lucas@192.168.1.181>
2018-11-21 20:43:00 -0500

    Andy: Wrote all encoder, lucas pushed cause of error on andys laptop

--- LA6/src/edu/wmich/cs1120/LA6/Encoder.java
+++ LA6/src/edu/wmich/cs1120/LA6/Encoder.java
@@ -1,11 +1,68 @@
 package edu.wmich.cs1120.LA6;
 
-public class Encoder implements IEncoder{
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.Random;
+import java.util.Scanner;
 
-	@Override
-	public void encode(String inputFileName, String outputFilePath) {
-		// TODO Auto-generated method stub
-		
-	}
+public class Encoder implements IEncoder {
+
+    @Override
+    public void encode(String inputFileName, String outputFilePath) {
+
+        Random rnd = new Random();
+        File inFile = new File(inputFileName);
+        String inputText = "";
+
+        try {
+            Scanner scanInFile = new Scanner(inFile);
+
+            while (scanInFile.hasNextLine()) {
+
+                if (inputText.compareTo("") != 0) {
+                    inputText = inputText + "\n";
+                }
+
+                inputText = inputText + scanInFile.nextLine();
+
+            }
+
+            scanInFile.close();
+
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Error while reading input: " + e.getMessage());
+        }
+
+        try {
+            RandomAccessFile outFile = new RandomAccessFile(outputFilePath, "rw");
+
+            char[] inputCharArr = inputText.toCharArray();
+            int rndOffset = 1;
+
+            for (int i = 0; i < inputCharArr.length; i++) {
+
+                outFile.writeChar(inputCharArr[i]);
+
+                if (i != (inputCharArr.length - 1)) {
+                    rndOffset = rnd.nextInt(20) + 1;
+                    outFile.writeInt(rndOffset);
+                    outFile.seek(outFile.getFilePointer() + rndOffset);
+                }
+
+            }
+
+            outFile.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Error while writing output: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Something modified the Char Array during encoding!!!");
+            e.printStackTrace();
+        }
+
+    }
 
 }
